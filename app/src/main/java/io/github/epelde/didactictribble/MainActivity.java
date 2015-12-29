e484820e6fa504acf781402cea4db772d4ad4cd4
Alex Epelde <aepelde@gmail.com>
2015-12-29 14:15:11 +0100

    MainActivity layout

--- app/src/main/java/io/github/epelde/didactictribble/MainActivity.java
+++ app/src/main/java/io/github/epelde/didactictribble/MainActivity.java
@@ -1,13 +1,26 @@
 package io.github.epelde.didactictribble;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mGenerateTicketButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGenerateTicketButton = (Button) findViewById(R.id.generate_ticket_button);
+        mGenerateTicketButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
