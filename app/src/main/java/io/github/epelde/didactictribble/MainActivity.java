72034f5de818669189dcd76685043df0be95fdee
Alex Epelde <aepelde@gmail.com>
2016-01-13 13:16:50 +0100

    Checking for Bluetooh enabled

--- app/src/main/java/io/github/epelde/didactictribble/MainActivity.java
+++ app/src/main/java/io/github/epelde/didactictribble/MainActivity.java
@@ -1,12 +1,17 @@
 package io.github.epelde.didactictribble;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -18,7 +23,10 @@ import retrofit.Response;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private final static int REQUEST_ENABLE_BT = 1;
+
     private Button mGenerateTicketButton;
+    private BluetoothAdapter btAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +36,50 @@ public class MainActivity extends AppCompatActivity {
         mGenerateTicketButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                new GenerateTicketTask().execute();
+                //new GenerateTicketTask().execute();
+                checkBluetoohEnabled();
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) {
+                printTickets();
+            } else {
+                Toast.makeText(this, getString(R.string.msg_bluetooh_not_enabled),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void checkBluetoohEnabled() {
+        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            btAdapter = ((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE))
+                    .getAdapter();
+            if (btAdapter != null) {
+                if (!btAdapter.isEnabled()) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                } else {
+                    printTickets();
+                }
+            } else {
+                Toast.makeText(this, getString(R.string.msg_bluetooh_adapter_available),
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, getString(R.string.msg_bluetooh_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void printTickets() {
+        Toast.makeText(this, "Listo para imprimir tickets", Toast.LENGTH_LONG).show();
+    }
+
 
     private void displayTicket(Ticket ticket) {
         Intent intent = new Intent(this, TicketActivity.class);
