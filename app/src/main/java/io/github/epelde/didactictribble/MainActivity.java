a5a207824b3da0d348f7756b63307f8c289460a6
Alex Epelde <aepelde@gmail.com>
2016-01-13 14:55:36 +0100

    Scanning for Bluetooh devices

--- app/src/main/java/io/github/epelde/didactictribble/MainActivity.java
+++ app/src/main/java/io/github/epelde/didactictribble/MainActivity.java
@@ -2,18 +2,28 @@ package io.github.epelde.didactictribble;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit.Call;
 import retrofit.Response;
@@ -27,11 +37,13 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mGenerateTicketButton;
     private BluetoothAdapter bluetoohAdapter;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler = new Handler();
         mGenerateTicketButton = (Button) findViewById(R.id.generate_ticket_button);
         mGenerateTicketButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -47,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_ENABLE_BT) {
             if (resultCode == RESULT_OK) {
-                printTickets();
+                scanForDevices();
             } else {
                 Toast.makeText(this, getString(R.string.msg_bluetooh_not_enabled),
                         Toast.LENGTH_SHORT).show();
@@ -64,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
                     Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                     startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
                 } else {
-                    printTickets();
+                    scanForDevices();
                 }
             } else {
                 Toast.makeText(this, getString(R.string.msg_bluetooh_adapter_available),
@@ -75,6 +87,60 @@ public class MainActivity extends AppCompatActivity {
                     Toast.LENGTH_SHORT).show();
         }
     }
+
+    private void scanForDevices() {
+        Toast.makeText(this, "Buscando impresora...", Toast.LENGTH_LONG).show();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            final BluetoothLeScanner scanner = bluetoohAdapter.getBluetoothLeScanner();
+            Log.i("XXX", "1");
+            final ScanCallback scanCallback = new ScanCallback() {
+                @Override
+                public void onScanResult(int callbackType, ScanResult result) {
+                    Log.i("XXX", "callbackType:" + String.valueOf(callbackType));
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        Log.i("XXX", "RESULT:" + result.toString());
+                    }
+                }
+
+                @Override
+                public void onBatchScanResults(List<ScanResult> results) {
+                    Log.i("XXX", "RESULTS!!!!");
+                    for (ScanResult sr : results) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                            Log.i("XXX", "ScanResult - Results" + sr.toString());
+                        }
+                    }
+                }
+
+                @Override
+                public void onScanFailed(int errorCode) {
+                    Log.e("XXX", "Scan failed");
+                }
+            };
+
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("XXX", "STOPPING");
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        scanner.stopScan(scanCallback);
+                    }
+                }
+            }, 10000);
+            Log.i("XXX", "2");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                Log.i("XXX", "3");
+                ScanSettings settings = new ScanSettings.Builder()
+                        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                        .build();
+                List<ScanFilter> filters = new ArrayList<ScanFilter>();
+                scanner.startScan(filters, settings, scanCallback);
+            }
+        } else {
+            // TODO Provide scanning method for older versions
+        }
+    }
+
 
     private void printTickets() {
         Toast.makeText(this, "Listo para imprimir tickets", Toast.LENGTH_LONG).show();
