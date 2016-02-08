563f891ffed507799e70390b573a614c575b83be
Alex Epelde <aepelde@gmail.com>
2016-02-08 13:11:03 +0100

    Listing devices

--- app/src/main/java/io/github/epelde/didactictribble/DeviceListActivity.java
+++ app/src/main/java/io/github/epelde/didactictribble/DeviceListActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2009 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.github.epelde.didactictribble;
 
 import android.app.Activity;
@@ -39,18 +23,11 @@ import com.zj.btsdk.BluetoothService;
 
 import java.util.Set;
 
-/**
- * This Activity appears as a dialog. It lists any paired devices and
- * devices detected in the area after discovery. When a device is chosen
- * by the user, the MAC address of the device is sent back to the parent
- * Activity in the result Intent.
- */
 public class DeviceListActivity extends Activity {
-    // Return Intent extra
-    public static String EXTRA_DEVICE_ADDRESS = "device_address";
 
-    // Member fields
-    BluetoothService mService = null;
+    public static final String EXTRA_DEVICE_ADDRESS = "didactictribble.device_address";
+
+    private BluetoothService service = null;
     private ArrayAdapter<String> mPairedDevicesArrayAdapter;
     private ArrayAdapter<String> mNewDevicesArrayAdapter;
 
@@ -96,10 +73,10 @@ public class DeviceListActivity extends Activity {
         filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
         this.registerReceiver(mReceiver, filter);
 
-        mService = new BluetoothService(this, null);
+        service = new BluetoothService(this, null);
 
         // Get a set of currently paired devices
-        Set<BluetoothDevice> pairedDevices = mService.getPairedDev();
+        Set<BluetoothDevice> pairedDevices = service.getPairedDev();
 
         // If there are paired devices, add each one to the ArrayAdapter
         if (pairedDevices.size() > 0) {
@@ -116,10 +93,10 @@ public class DeviceListActivity extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (mService != null) {
-        	mService.cancelDiscovery();
+        if (service != null) {
+        	service.cancelDiscovery();
         }
-        mService = null;
+        service = null;
         this.unregisterReceiver(mReceiver);
     }
 
@@ -136,19 +113,19 @@ public class DeviceListActivity extends Activity {
         findViewById(R.id.title_new_devices).setVisibility(View.VISIBLE);
 
         // If we're already discovering, stop it
-        if (mService.isDiscovering()) {
-        	mService.cancelDiscovery();
+        if (service.isDiscovering()) {
+        	service.cancelDiscovery();
         }
 
         // Request discover from BluetoothAdapter
-        mService.startDiscovery();
+        service.startDiscovery();
     }
 
     // The on-click listener for all devices in the ListViews
     private OnItemClickListener mDeviceClickListener = new OnItemClickListener() {   //����б�������豸
         public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
             // Cancel discovery because it's costly and we're about to connect
-        	mService.cancelDiscovery();
+        	service.cancelDiscovery();
 
             // Get the device MAC address, which is the last 17 chars in the View
             String info = ((TextView) v).getText().toString();
