a66f2eabfdb11e261d29e1172d1cec90f6f8ad3c
Alex Epelde <aepelde@gmail.com>
2016-02-10 13:02:47 +0100

    Displaying ticket printing button

--- app/src/main/java/io/github/epelde/didactictribble/MainActivity.java
+++ app/src/main/java/io/github/epelde/didactictribble/MainActivity.java
@@ -4,9 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by epelde on 08/02/2016.
@@ -15,18 +15,31 @@ public class MainActivity extends Activity {
 
     private static final int REQUEST_CODE_SELECT_DEVICE = 1;
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    private Button configBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        configBtn = (Button) findViewById(R.id.config_button);
+        Button configBtn = (Button) findViewById(R.id.config_button);
         configBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivityForResult(new Intent(MainActivity.this, DeviceListActivity.class),
                         REQUEST_CODE_SELECT_DEVICE);
+            }
+        });
+        Button printBtn = (Button) findViewById(R.id.print_button);
+        printBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String address = getPreferences(MODE_PRIVATE).getString(
+                        getString(R.string.KEY_PRINTER_ADRESS), null);
+                // checking if there is mad address stored
+                if (address == null) {
+                    Toast.makeText(MainActivity.this, R.string.toast_msg_no_printer, Toast.LENGTH_SHORT).show();
+                } else {
+
+                }
             }
         });
     }
@@ -39,9 +52,6 @@ public class MainActivity extends Activity {
                 if (resultCode == Activity.RESULT_OK) {
                     String address = data.getExtras()
                             .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
-                    Log.i(LOG_TAG, "* * * * * * * * * * * * * * * *");
-                    Log.i(LOG_TAG, "* * * ADDRESS:" + address);
-                    Log.i(LOG_TAG, "* * * * * * * * * * * * * * * *");
                     SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
                     editor.putString(getString(R.string.KEY_PRINTER_ADRESS), address);
                     editor.commit();
