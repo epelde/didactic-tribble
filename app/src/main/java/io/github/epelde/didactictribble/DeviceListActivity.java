4b572d1558e4f6401ca4bdfe5c312172f5f84b82
Alex Epelde <aepelde@gmail.com>
2016-02-10 12:19:21 +0100

    Listing paired devices and saving preferences

--- app/src/main/java/io/github/epelde/didactictribble/DeviceListActivity.java
+++ app/src/main/java/io/github/epelde/didactictribble/DeviceListActivity.java
@@ -8,10 +8,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -25,22 +23,17 @@ import java.util.Set;
 
 public class DeviceListActivity extends Activity {
 
+    private static final int REQUEST_CODE_ENABLE_BT = 1;
     public static final String EXTRA_DEVICE_ADDRESS = "didactictribble.device_address";
 
     private BluetoothService service = null;
-    private ArrayAdapter<String> mPairedDevicesArrayAdapter;
-    private ArrayAdapter<String> mNewDevicesArrayAdapter;
+    private ArrayAdapter<String> devicesArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Setup the window
-        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_device_list);
-
-        // Set result CANCELED incase the user backs out
-        setResult(Activity.RESULT_CANCELED);
-
+        setResult(Activity.RESULT_CANCELED); // Set result CANCELED in case the user backs out
         // Initialize the button to perform device discovery
         Button scanButton = (Button) findViewById(R.id.button_scan);
         scanButton.setOnClickListener(new OnClickListener() {
@@ -50,120 +43,83 @@ public class DeviceListActivity extends Activity {
             }
         });
 
-        // Initialize array adapters. One for already paired devices and
-        // one for newly discovered devices
-        mPairedDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
-        mNewDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
-        
-        // Find and set up the ListView for paired devices
-        ListView pairedListView = (ListView) findViewById(R.id.paired_devices);
-        pairedListView.setAdapter(mPairedDevicesArrayAdapter);
-        pairedListView.setOnItemClickListener(mDeviceClickListener);
-
-        // Find and set up the ListView for newly discovered devices
-        ListView newDevicesListView = (ListView) findViewById(R.id.new_devices);
-        newDevicesListView.setAdapter(mNewDevicesArrayAdapter);
-        newDevicesListView.setOnItemClickListener(mDeviceClickListener);
+        devicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
+        ListView devicesListView = (ListView) findViewById(R.id.devices_list_view);
+        devicesListView.setAdapter(devicesArrayAdapter);
+        devicesListView.setOnItemClickListener(deviceClickListener);
+        service = new BluetoothService(this, null);
+        Set<BluetoothDevice> pairedDevices = service.getPairedDev();
+        if (!pairedDevices.isEmpty()) {
+            for (BluetoothDevice device : pairedDevices) {
+                devicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
 
         // Register for broadcasts when a device is discovered
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        this.registerReceiver(mReceiver, filter);
-
+        this.registerReceiver(receiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
         // Register for broadcasts when discovery has finished
-        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        this.registerReceiver(mReceiver, filter);
-
-        service = new BluetoothService(this, null);
-
-        // Get a set of currently paired devices
-        Set<BluetoothDevice> pairedDevices = service.getPairedDev();
-
-        // If there are paired devices, add each one to the ArrayAdapter
-        if (pairedDevices.size() > 0) {
-            findViewById(R.id.title_paired_devices).setVisibility(View.VISIBLE);
-            for (BluetoothDevice device : pairedDevices) {
-                mPairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-            }
-        } else {
-            String noDevices = getResources().getText(R.string.none_paired).toString();
-            mPairedDevicesArrayAdapter.add(noDevices);
-        }
+        this.registerReceiver(receiver,
+                new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED));
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         if (service != null) {
-        	service.cancelDiscovery();
+            service.cancelDiscovery();
         }
         service = null;
-        this.unregisterReceiver(mReceiver);
+        this.unregisterReceiver(receiver);
     }
 
     /**
      * Start device discover with the BluetoothAdapter
      */
     private void doDiscovery() {
-
         // Indicate scanning in the title
         setProgressBarIndeterminateVisibility(true);
         setTitle(R.string.scanning);
-
-        // Turn on sub-title for new devices
-        findViewById(R.id.title_new_devices).setVisibility(View.VISIBLE);
-
         // If we're already discovering, stop it
         if (service.isDiscovering()) {
-        	service.cancelDiscovery();
+            service.cancelDiscovery();
         }
-
         // Request discover from BluetoothAdapter
         service.startDiscovery();
     }
 
     // The on-click listener for all devices in the ListViews
-    private OnItemClickListener mDeviceClickListener = new OnItemClickListener() {   //����б�������豸
+    private OnItemClickListener deviceClickListener = new OnItemClickListener() {
         public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
-            // Cancel discovery because it's costly and we're about to connect
-        	service.cancelDiscovery();
-
+            // Cancel discovery because it's costly and we have done selection!!!
+            service.cancelDiscovery();
             // Get the device MAC address, which is the last 17 chars in the View
             String info = ((TextView) v).getText().toString();
             String address = info.substring(info.length() - 17);
-
-            // Create the result Intent and include the MAC address
             Intent intent = new Intent();
             intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
-            Log.d("���ӵ�ַ", address);
-            
-            // Set result and finish this Activity
             setResult(Activity.RESULT_OK, intent);
             finish();
         }
     };
 
-    // The BroadcastReceiver that listens for discovered devices and
-    // changes the title when discovery is finished
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+    // The BroadcastReceiver that listens for discovered devices
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
-            
             // When discovery finds a device
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Get the BluetoothDevice object from the Intent
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 // If it's already paired, skip it, because it's been listed already
                 if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
-                    mNewDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                    devicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
                 }
-            // When discovery is finished, change the Activity title
             } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
                 setProgressBarIndeterminateVisibility(false);
                 setTitle(R.string.select_device);
-                if (mNewDevicesArrayAdapter.getCount() == 0) {
+                if (devicesArrayAdapter.getCount() == 0) {
                     String noDevices = getResources().getText(R.string.none_found).toString();
-                    mNewDevicesArrayAdapter.add(noDevices);
+                    devicesArrayAdapter.add(noDevices);
                 }
             }
         }
--- app/src/main/java/io/github/epelde/didactictribble/MainActivity.java
+++ app/src/main/java/io/github/epelde/didactictribble/MainActivity.java
@@ -2,7 +2,9 @@ package io.github.epelde.didactictribble;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +13,8 @@ import android.widget.Button;
  */
 public class MainActivity extends Activity {
 
+    private static final int REQUEST_CODE_SELECT_DEVICE = 1;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private Button configBtn;
 
     @Override
@@ -21,8 +25,28 @@ public class MainActivity extends Activity {
         configBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ConfigActivity.class));
+                startActivityForResult(new Intent(MainActivity.this, DeviceListActivity.class),
+                        REQUEST_CODE_SELECT_DEVICE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CODE_SELECT_DEVICE:
+                if (resultCode == Activity.RESULT_OK) {
+                    String address = data.getExtras()
+                            .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+                    Log.i(LOG_TAG, "* * * * * * * * * * * * * * * *");
+                    Log.i(LOG_TAG, "* * * ADDRESS:" + address);
+                    Log.i(LOG_TAG, "* * * * * * * * * * * * * * * *");
+                    SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+                    editor.putString(getString(R.string.KEY_PRINTER_ADRESS), address);
+                    editor.commit();
+                }
+                break;
+        }
     }
 }
